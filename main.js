0c201243a11e20858ffe2c118b0e327d70a1facf
waxhsu <98721786+waxhsu@users.noreply.github.com>
2024-01-21 16:51:10 +0800

    finally works
    replaced axios and openai folders in node_modules

--- main.js
+++ main.js
@@ -1,36 +1,53 @@
-
 const { Configuration, OpenAIApi } = require("openai");
 
 const config = new Configuration({
-    apiKey: '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',
-})
+	apiKey: "sk-APIKEY",
+});
 
 const openai = new OpenAIApi(config);
 
 const runPrompt = async () => {
-    const prompt = "Tell me a joke about a cat eating pizza.";
+	const prompt = `
+        You are an employer's assistant and tasked to write humorously vague and unrealistic job requirements. Make sure that the position is very mundane.
+        
+        For About, write 2-3 sentences about the job position. Avoid using question marks.
+        
+        For Position, the job position names are to be absurd and kind of stupid. Avoid using "Chief" and "Officer".
+        
+        For Location, provide a random city and country. Stay within North America.
 
-    const response = await openai.createCompletion({
-        model: "gpt-4-1106-preview",
-        prompt: prompt,
-        max_tokens: 2048,
-        temperature: 1,
-    });
+        For Responsibilities and Qualifications, list four bullet points in an array. Must have one bullet point that is actually realistic. Avoid using internet speak and "unicorns". 
+        
+        Provide a number value between 40000 to 100000 for compensation.
 
-    console.log(response.data)
-}
+        Return response in the following parsable JSON format:
+
+        {
+          "P": "position",
+          "A": "about",
+          "L": "location",
+          "R": "responsibilities",
+          "Q": "qualifications",
+          "C": "compensation",
+        }
+    `;
+
+	const response = await openai.createCompletion({
+		model: "gpt-3.5-turbo-instruct",
+		prompt: prompt,
+		max_tokens: 2048,
+		temperature: 1,
+	});
+
+	const parsableJSONresponse = response.data.choices[0].text;
+	const parsedResponse = JSON.parse(parsableJSONresponse);
+
+	console.log("Position: ", parsedResponse.P);
+	console.log("About: ", parsedResponse.A);
+	console.log("Location: ", parsedResponse.L);
+	console.log("Responsibilities: ", parsedResponse.R);
+	console.log("Qualifications: ", parsedResponse.Q);
+	console.log("Compensation: $", parsedResponse.C);
+};
 
 runPrompt();
-
-// import OpenAI from "openai";
-
-// const openai = new OpenAI({
-//     apiKey: "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",
-// });
-
-// const chatCompletion = await openai.chat.completions.create({
-//     messages: [{ 
-//         role: "user", 
-//         content: "Say this is a test" }],
-//     model: "gpt-3.5-turbo",
-// });
